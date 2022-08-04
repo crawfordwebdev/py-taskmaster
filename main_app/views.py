69588aa0b23e8b620bb5ba624db2d7de6e0a2eab
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-08-03 21:49:31 -0600

    adds database objects

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,5 @@
 from django.shortcuts import render
-
-
-# Add the Cat class & list and view function below the imports
-class Task:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-tasks = [
-  Task('Lolo', 'tabby', 'Kinda rude.', 3),
-  Task('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-  Task('Fancy', 'bombay', 'Happy fluff ball.', 4),
-  Task('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
-
+from .models import Task
 
 def home(request):
   return render(request, 'home.html')
@@ -24,4 +8,5 @@ def about(request):
   return render(request, 'about.html')
 
 def tasks_index(request):
+  tasks = Task.objects.all()
   return render(request, 'tasks/index.html', { 'tasks': tasks })
