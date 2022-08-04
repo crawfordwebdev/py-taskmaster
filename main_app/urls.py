1abce7be31e53f19d62ab84609404769d4dd07cc
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-08-03 22:25:25 -0600

    adds create, update, delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('tasks/', views.tasks_index, name='tasks_index'),
   path('tasks/<int:task_id>/', views.tasks_detail, name='tasks_detail'),
+  path('tasks/create/', views.TaskCreate.as_view(), name='tasks_create'),
+  path('tasks/<int:pk>/update/', views.TaskUpdate.as_view(), name='tasks_update'),
+  path('tasks/<int:pk>/delete/', views.TaskDelete.as_view(), name='tasks_delete'),
 ]
