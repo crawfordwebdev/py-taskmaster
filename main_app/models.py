f744bd97d33e04d46aba1ad0a1fee7f06880e4b8
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-08-05 12:28:06 -0600

    adds penalty and rewards

--- main_app/models.py
+++ main_app/models.py
@@ -9,12 +9,32 @@ PRIORITY = (
   ('4', 'Emergency')
 )
 
+class Reward(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('reward_detail', kwargs={'pk': self.id})
+
+class Penalty(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('penalty_detail', kwargs={'pk': self.id})
+
 class Task(models.Model):
   date = models.DateField(blank=True),
   name = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
-  reward = models.CharField(max_length=100)
-  penalty = models.CharField(max_length=100)
+  reward = models.ManyToManyField(Reward, blank=True)
+  penalty = models.ManyToManyField(Penalty, blank=True)
   priority = models.CharField(
     max_length=1,
     choices=PRIORITY,
@@ -27,3 +47,9 @@ class Task(models.Model):
 
   def get_absolute_url(self):
     return reverse('tasks_detail', kwargs={'task_id': self.id})
+
+  def get_priority(self):
+    return f"{self.get_priority_display()}"
+
+
+
